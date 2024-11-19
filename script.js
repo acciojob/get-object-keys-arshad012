2ffb80cfb4054f4210ba682a221d8c6d2eeadae1
arshad012 <forarshad99@gmail.com>
2024-11-19 22:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 //your JS code here. If required.
+/*
 const myObj = {
 	name:'John',
 	age:30,
 	city:'New York',
-	// getKeys: function() {
-	// 	let keysArr = [];
-	// 	for(let key in this) {
-	// 		keysArr.push(key);
-	// 	}
-	// 	keysArr.pop();
-	// 	return keysArr;
-	// } 
+	getKeys: function() {
+		let keysArr = [];
+		for(let key in this) {
+			keysArr.push(key);
+		}
+		keysArr.pop();
+		return keysArr;
+	} 
 }
+*/
 
+/*
 function getKeys() {
 	let keysArr = [];
 	for(let key in this) {
 		keysArr.push(key);
 	}
-	// keysArr.pop();
 	return keysArr;
 } 
 console.log(getKeys.call(myObj));
-// getKeys.call(myObj);
+getKeys.call(myObj);
+*/
+
 
 /*
 class Obj {
