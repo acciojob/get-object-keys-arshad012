8b4ebee5c123a59803dc3b8f2ccd4b2f54ee955a
arshad012 <forarshad99@gmail.com>
2024-11-19 18:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 //your JS code here. If required.
 class Obj {
-	constructor(name, age) {
+	constructor(name, age, city) {
 		this.name = name;
 		this.age = age;
+		this.city = city;
 	}
 
 	getKeys() {
-		// let keysArr = [];
-		// for(let key in this) {
-		// 	keysArr.push(key);
-		// }
-		return this;
+		let keysArr = [];
+		for(let key in this) {
+			keysArr.push(key);
+		}
+		return keysArr;
 	}
 }
 
-const newObj = new Obj('Arshad', 25);
-console.log(newObj.getKeys());
+const myObj = new Obj("John", 30, "New York");
+const keys = myObj.getKeys();
+console.log(keys);
