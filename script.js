ed40bae71f3d149dac5f2e0d5b1d4f8d20e78a4f
arshad012 <forarshad99@gmail.com>
2024-11-26 12:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,18 @@
 //your JS code here. If required.
-/*
+
 const myObj = {
 	name:'John',
 	age:30,
 	city:'New York',
-	getKeys: function() {
-		let keysArr = [];
-		for(let key in this) {
-			keysArr.push(key);
-		}
-		keysArr.pop();
-		return keysArr;
-	} 
 }
-*/
 
-/*
-function getKeys() {
+myObj.__proto__.getKeys = function() {
 	let keysArr = [];
 	for(let key in this) {
 		keysArr.push(key);
 	}
+	keysArr.pop();
 	return keysArr;
-} 
-console.log(getKeys.call(myObj));
-getKeys.call(myObj);
-*/
-
-
-/*
-class Obj {
-	constructor(name, age, city) {
-		this.name = name;
-		this.age = age;
-		this.city = city;
-	}
-
-	getKeys() {
-		let keysArr = [];
-		for(let key in this) {
-			keysArr.push(key);
-		}
-		return keysArr;
-	}
 }
 
-const myObj = new Obj("John", 30, "New York");
-const keys = myObj.getKeys();
-console.log(keys);
-*/
-
+console.log(myObj.getKeys())
